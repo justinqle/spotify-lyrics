230b971eb0189d52b691e4332cc29de1607fbde3
Justin Le <justinqle@gmail.com>
2019-01-09 01:03:51 -0500

    Application is completely functional

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
+#!/usr/bin/env python3.7
+
 import os
 import sys
 import spotipy
 import spotipy.util as util
+import lyricsgenius
 
 scope = 'user-read-currently-playing'
-username = os.getenv('SPOTIFY_USERNAME')  # environment variable for Spotify username
+username = os.getenv('SPOTIFY_USERNAME')  # environment variable specifying Spotify username
 if username is None:
-    print("Please specify Spotify username in environment variable.")
+    print("Please specify Spotify username in an environment variable called SPOTIFY_USERNAME.")
     sys.exit(1)
 
 token = util.prompt_for_user_token(username=username,
@@ -14,6 +17,13 @@ token = util.prompt_for_user_token(username=username,
                                    client_id='762e2be7c3bf47fb94c4fc3aa210647f',
                                    client_secret='42b74dea41ff48ffafb7f00eb9a3aaf5',
                                    redirect_uri='http://localhost/')
-spotify = spotipy.Spotify(auth=token)
+spotify = spotipy.Spotify(auth=token)  # Spotipy API wrapper
 
-print(spotify.currently_playing())
+playing = spotify.currently_playing()
+name = playing['item']['name']
+artist = playing['item']['artists'][0]['name']
+
+genius = lyricsgenius.Genius('H5mBdF6qCxfIjMFXY8pTFw-0vxFInPmJyJdpzF0Wc5sxEE7ECS4RikIrhv1JoDA_')  # Genius API wrapper
+song = genius.search_song(name, artist)
+print()
+print(song.lyrics)
