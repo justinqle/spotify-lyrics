dfb89c2b332daeb4b6b901e14c5f371c65efed80
Justin Le <justinqle@gmail.com>
2019-02-28 02:36:13 -0500

    Quality of life & error checking

--- spotify-lyrics.py
+++ spotify-lyrics.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3.7
 
 import os
+import subprocess
 import sys
 import spotipy
 import spotipy.util as util
@@ -12,24 +13,35 @@ if username is None:
     print("Please specify Spotify username in an environment variable called SPOTIFY_USERNAME.")
     sys.exit(1)
  
-# Put Spotify credentials here
-client_id = ""
-client_secret = ""
-# Put Genius API key
-api_key = ""
+# Put Spotify developer credentials here
+spotify_id = ""
+spotify_secret = ""
+# Put Genius developer credentials here
+genius_secret = ""
 
 token = util.prompt_for_user_token(username=username,
                                    scope=scope,
-                                   client_id=client_id,
-                                   client_secret=client_secret,
+                                   client_id=spotify_id,
+                                   client_secret=spotify_secret,
                                    redirect_uri='http://localhost/')
+
 spotify = spotipy.Spotify(auth=token)  # Spotipy API wrapper
 
-playing = spotify.currently_playing()
+try:
+    playing = spotify.currently_playing()
+except Exception:
+    print("Network error, please verify connection.")
+    sys.exit(1)
+
 name = playing['item']['name']
+# Song names with parentheses
+name = name.split('(')[0].rstrip()
 artist = playing['item']['artists'][0]['name']
 
-genius = lyricsgenius.Genius(api_key)  # Genius API wrapper
+genius = lyricsgenius.Genius(genius_secret)  # Genius API wrapper
 song = genius.search_song(name, artist)
-print()
-print(song.lyrics)
+if song is not None:
+    rows, columns = subprocess.check_output(['stty', 'size']).decode().split()
+    columns = int(columns)
+    print('-' * columns)
+    print(song.lyrics)
